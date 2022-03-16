39e04d996d770e46d2706631ada5084af51a891f
JasperEssien2 <jasperessien2@gmail.com>
2022-03-16 22:45:01 +0100

    proposed fixed for client side app not connecting to endpoint

--- music_mates/urls.py
+++ music_mates/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 from music_mates.schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
 
     # Only a single URL to access GraphQL
-    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema))
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)))
 ]
